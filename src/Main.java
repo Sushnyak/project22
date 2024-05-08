b6379d42037d5986ace7212da9d91a41d1f54576
Даниил <danilazakharov2003@gmail.com>
2024-05-08 19:47:21 +0300

    commit2

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        int cash = 300;
+        int cash = 1100;
         int bonus = 0;
         if (cash > 1000) {
             bonus = cash / 100;
         }
         cash = cash + bonus;
         System.out.println("Баланс: " + cash);
+        System.out.println("Бонус: " + bonus);
 
         /* Там у вас в задании написано, что при пополнении на 1100 рублей
            получается 11 бонусов и баланс должен быть 1211, хотя 1100 + 11 = 1111,
